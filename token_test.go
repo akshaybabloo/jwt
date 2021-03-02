ec205ee0b9b34b56971c2a0d61fcc134e97e7bcf
Akshay <akshaybabloo@gmail.com>
2021-03-02 16:40:28 +1300

    tests updated

--- token_test.go
+++ token_test.go
@@ -1,6 +1,7 @@
 package jwt
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -8,14 +9,11 @@ import (
 )
 
 func TestUnixTimeToTime(t *testing.T) {
-	unixTimeStamp := "1614581683"
+	unixTimeStamp := 1614581683
 	output, _ := time.Parse(time.RFC3339, "2021-03-01T19:54:43+13:00")
 
-	toTime, err := UnixTimeToTime(unixTimeStamp)
-	t.Log(toTime)
-	 if assert.Nil(t, err) {
-	 	assert.Equal(t, toTime, output)
-	 }
+	toTime := UnixTimeToTime(int64(unixTimeStamp))
+	assert.Equal(t, toTime, output)
 }
 
 func TestInTimeSpan(t *testing.T) {
@@ -25,4 +23,13 @@ func TestInTimeSpan(t *testing.T) {
 
 	span := InTimeSpan(t1, t3, t2)
 	assert.True(t, span)
+}
+
+func TestToken_Valid(t *testing.T) {
+
+	tk := Token{
+		AccessToken: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MTQ1ODY5ODIsIm5iZiI6MTYxNDU4MzM4MiwidmVyIjoiMS4wIiwiaXNzIjoiaHR0cHM6Ly9nb2xsYWhhbGxpYXV0aC5iMmNsb2dpbi5jb20vNDkyMDY1NmQtMzZlMC00YjM1LTg5NGQtMGEwYTE0YmVjOGIzL3YyLjAvIiwic3ViIjoiNzYyNWY0MDktMWZhNy00ZDc4LTkzYjAtNWMzNDAyNTVjZGYyIiwiYXVkIjoiMTIzNDU2Nzg5Iiwibm9uY2UiOiJkZWZhdWx0Tm9uY2UiLCJpYXQiOjE2MTQ1ODMzODIsImF1dGhfdGltZSI6MTYxNDU4MzM4Miwib2lkIjoic29tZS1vaWQiLCJnaXZlbl9uYW1lIjoiQWtzaGF5IiwiZmFtaWx5X25hbWUiOiJHb2xsYWhhbGxpIiwidGZwIjoiQjJDXzFfU2lnblVwU2lnbkluRmxvdyJ9.CNbe3qUhpUR7U51e9k1miAdQP3ZEbCDip67MlvgTpFsFmB3nbjp7wi8e-66cPoS9z_hmQP3wLc5I8KE-b4_cFCzHZkhuQPA0Mhi9mBAZ_tAPSXNaDeiX1FEalsiH_sWuHWnojxSwlSxeKL9Tlh_0u5vXaABILdDeRWOTBJDHZ5I2BgIk8J_hI-fXDvBb0wfjI4mQe7lQqDZLVos4mlA1Uhpz2wN7Lorc31PZo02STbk5S1j1BMk4eQUS_OHTKZfAlUmfV9giWEMr7qk21eSy_HlybVKQgCB9Cde_rmNWJETyw6X712QGYWaDkDrwocQ99wR6rALWVkkTOhmL6CRT5Q",
+	}
+	fmt.Println(tk.Valid())
+
 }
