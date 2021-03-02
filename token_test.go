8f9e09ed453b4f88dcef0131cbfdc0de466abe8b
Akshay <akshaybabloo@gmail.com>
2021-03-02 20:25:04 +1300

    unix time test removed

--- token_test.go
+++ token_test.go
@@ -8,14 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUnixTimeToTime(t *testing.T) {
-	unixTimeStamp := 1614581683
-	output, _ := time.Parse(time.RFC3339, "2021-03-01T19:54:43+13:00")
-
-	toTime := UnixTimeToTime(int64(unixTimeStamp))
-	assert.Equal(t, toTime, output)
-}
-
 func TestInTimeSpan(t *testing.T) {
 	t1 := time.Date(1984, time.November, 3, 10, 23, 34, 0, time.UTC)
 	t2 := time.Date(1984, time.November, 3, 11, 23, 34, 0, time.UTC)
